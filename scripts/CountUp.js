a5e54ca94c9cf2caf9e15fc0fff021a3caec38c5
Miki <mlptwitteranon@gmail.com>
2021-06-30 18:06:30 +0100

    Add files via upload

---
+++ scripts/CountUp.js
@@ -0,0 +1,33 @@
+var a;
+
+//displayed time & date difference
+var dyear;
+var dmonth;
+var dday;
+var dhr;
+var dmin;
+var dsec;
+
+var count = document.querySelector('#countup');
+
+window.onload = function() {
+	datetime_diff();
+    loopstats();
+};
+
+//increments countup
+function loopstats() {
+	a = setInterval(datetime_diff, 1000);
+};
+
+function datetime_diff() {
+	var then = moment('2021-07-01 12:00:00');
+	// Original Date: 2016-07-10 16:25:00
+	// Date adjusted for time apart because I'm lazy and don't want to edit the code
+	var now = moment();
+	var diff = moment.preciseDiff(then, now);
+	count.innerHTML = diff;
+	
+};
+	count.innerHTML = (dyear + " Years") + ', ' + (dmonth + " Months") + ', ' + (dday + " Days") + ("0" + dhr).slice(-2) + ' Hours, ' + ("0" + dmin).slice(-2) + ' Minutes, ' + ("0" + dsec).slice(-2) + ' Seconds';
+
